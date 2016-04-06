02a98c894bd4aa46a8c72e5379ba627b84aae74a
mithunvp <mithun.pattankar@bsil.com>
2016-04-06 10:14:05 +0530

    Initial Commit

---
+++ src/ContactsApi/Repository/IContactsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContactsApi.Models;
+using System.Collections.Generic;
+
+namespace ContactsApi.Repository
+{
+    public interface IContactsRepository
+    {
+        void Add(Contacts item);
+        IEnumerable<Contacts> GetAll();
+        Contacts Find(string key);
+        void Remove(string Id);
+        void Update(Contacts item);
+    }
+}
